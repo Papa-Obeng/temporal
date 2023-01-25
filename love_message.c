bea99947e0e5d8b7dc330191021180119e034b3f
Papa-Obeng <papaobengn@gmail.com>
2023-01-25 18:06:50 +0000

    Love

--- love_message.c
+++ love_message.c
@@ -6,11 +6,20 @@
  */
 int main(void)
 {
-	char message[][] = {{'73'}, {'108''111''118''101'}, {'121''111''117'},
-		{'102''114''111''109'}};
-	char eman[] = {'84''105''109'};
+	int x;
+	char message[15] = {73, 108, 111, 118, 101, 121, 111,
+		117, 102, 114, 111, 109, 84, 105,
+		109};
 
-	printf("%s\ %s", message, eman);
+	for (x = 0; x <= message[15]; x++)
+	{
+		if (message[x] == 0 || message[x] == 4 || message[x] == 7 || 
+				message[x] == 11)
+		{
+			putchar(' ');
+		} else
+			printf("%c", message[x]);
+	}
 
 	return (0);
 }
